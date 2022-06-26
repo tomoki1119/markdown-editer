7daf806f9ae344b4ee0d46d54f6b65555e924bb8
tomoki1119 <kanon.dmc@gmail.com>
2022-06-27 01:01:12 +0900

    react開始

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
     entry:'./src/index.tsx',
     module:{
         rules:[
             {
-                test:/\.tsx$/,
+                test:/\.tsx?$/,
                 use:'ts-loader',
                 exclude:/node_modules/,
             },
@@ -18,5 +19,25 @@ module.exports = {
         path:path.resolve(__dirname,'dist'),
         filename:'index.js',
         publicPath:'dist/',
-    }
+    },
+    devServer:{
+        static:{
+            directory: path.join(__dirname, 'dist')
+        },
+        hot:true,
+        open:true
+    },
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
 }
